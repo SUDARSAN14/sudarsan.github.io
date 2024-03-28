9543074d2d76f817143a9e98d571f40b04b61dc1
SUDARSAN <85564634+SUDARSAN14@users.noreply.github.com>
2024-03-28 11:36:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function togglemenu() {
+    const menu = document.querySelector(".item");
+    const icon = document.querySelector(".menuicon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
